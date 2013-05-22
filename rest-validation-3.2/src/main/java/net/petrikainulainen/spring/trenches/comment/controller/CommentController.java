77c03ca1a581e70571467b16444f65177d2f9209
Petri Kainulainen <petri.kainulainen@gmail.com>
2013-05-23 00:09:57 +0300

    Removed unnecessary code and dependencies

--- rest-validation-3.2/src/main/java/net/petrikainulainen/spring/trenches/comment/controller/CommentController.java
+++ rest-validation-3.2/src/main/java/net/petrikainulainen/spring/trenches/comment/controller/CommentController.java
@@ -1,22 +1,12 @@
 package net.petrikainulainen.spring.trenches.comment.controller;
 
 import net.petrikainulainen.spring.trenches.comment.dto.CommentDTO;
-import net.petrikainulainen.spring.trenches.comment.dto.ValidationErrorDTO;
-import net.petrikainulainen.spring.trenches.common.util.LocaleContextHolderWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
-import java.util.Locale;
 
 /**
  * @author Petri Kainulainen
