6da4f1efbc60501bc37700d253eeb738524160d1
Petri Kainulainen <petri.kainulainen@gmail.com>
2013-06-14 20:44:14 +0300

    Removed foo

--- foo/model/src/main/java/net/petrikainulainen/spring/datajpa/todo/model/Person.java
+++
@@ -1,7 +0,0 @@
-package net.petrikainulainen.spring.datajpa.todo.model;
-
-/**
- * @author Petri Kainulainen
- */
-public class Person {
-}
--- foo/service/src/main/java/net/petrikainulainen/spring/datajpa/person/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package net.petrikainulainen.spring.datajpa.person.repository;
-
-/**
- * @author Petri Kainulainen
- */
-public class PersonRepository {
-}
